3df4c6022711c4bdd7914194eee75bad3928762c
galib937 <mgalib937@gmail.com>
2024-04-25 01:06:02 +0600

    new method for checking the password validity from laptop

--- src/test/java/net/therapconnect/testing/ContactManagerTest.java
+++ src/test/java/net/therapconnect/testing/ContactManagerTest.java
@@ -1,10 +1,18 @@
 package net.therapconnect.testing;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ContactManagerTest {
+    @BeforeAll
+    static void shouldRunBeforeAll(){
+        System.out.println("Running Before anything has been run");
+    }
+    @AfterAll
+    static void shouldRunAfterAll(){
+        System.out.println("Running After everything has done executing");
+    }
     @Test
     void FirstnameShouldBeString(){
         var cm = new ContactManager();
@@ -22,8 +30,8 @@ public class ContactManagerTest {
         assertTrue(cm.determinePhoneNumberIsAString("987654321"));
     }
 
-
-    @Test
+    @DisplayName("Testing the repeated tests")
+    @RepeatedTest(value = 10)
     void passwordShouldBeValid(){
         var cm = new ContactManager();
         assertTrue(cm.passwordMustBeValid("acakjh34"));
